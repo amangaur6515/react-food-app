2824cfdb414c899b72ad0372cd8f66747c597e51
amangaur6515 <amangaur6515@gmail.com>
2023-11-16 23:13:40 +0530

    added store

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, createSlice } from "@reduxjs/toolkit";
+
+const cartSlice=createSlice({
+    name:'cart',
+    initialState:{
+        items:["banana","apple"]
+    },
+    reducers:{
+        //mapping between action method and reducer function
+        addItem:(state,action)=>{
+            state.items.push(action.payload)
+        },
+        removeItem:(state,action)=>{
+            state.items.pop()
+        },
+        clearCart:(state)=>{
+            state.items=[]
+        }
+    }
+})
+
+export const {addItem,removeItem,clearCart}=cartSlice.actions;
+export default cartSlice.reducer;
